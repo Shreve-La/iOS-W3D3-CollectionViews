53ceaba20e035f0822cce1d09943abb1a4947465
steve <steven.thompson.ca@gmail.com>
2016-11-15 13:32:47 -0500

    Initial Commit

---
+++ MusicCollection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MusicCollection
+//
+//  Created by steve on 2016-11-15.
+//  Copyright © 2016 steve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
